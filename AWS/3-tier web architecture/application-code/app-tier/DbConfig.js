5c4eb9a3703c7a970d0f71e08b356872905405ba
sharmaashish63 <sharma.ashish63@gmail.com>
2024-06-07 14:23:59 +1200

    update new project

--- AWS/3-tier web architecture/application-code/app-tier/DbConfig.js
+++ AWS/3-tier web architecture/application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     DB_HOST : 'database-1-instance-1.cznsyzczkkou.us-east-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'Rejpedscr&82',
-    DB_DATABASE : 'webappdb'
+    DB_PWD : '**************',
+    DB_DATABASE : 'webappdb' ccc
 });
